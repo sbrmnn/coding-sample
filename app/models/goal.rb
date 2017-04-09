5e7e2f14c160cda084a1d8fa66a63796aa96cab0
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-04-09 16:05:56 -0400

    Modified goal model

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,10 +1,9 @@
 class Goal < ApplicationRecord
   belongs_to :user
 
-  validates_presence_of :user, :name, :amount, :priority
-  validates_uniqueness_of :name
+  validates_presence_of :user
   validates :name, uniqueness: { scope: :user_id }
-  validates :completion, :numericality => { :greater_than => 0, :less_than_or_equal_to => 100 }
-  validates :priority, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 10 }
-  validates :amount, :numericality => { :greater_than => 0}
+  validates :completion, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
+  validates :priority, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
+  validates :amount, numericality: { greater_than: 0}
 end
