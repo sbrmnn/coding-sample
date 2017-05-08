3526ae0726a1dc39d95564e2887edff6e800d670
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-05-07 22:39:53 -0400

    Refactored routes and added swagger

--- config/routes.rb
+++ config/routes.rb
@@ -17,21 +17,15 @@ Rails.application.routes.draw do
   post '/cron/get_transfer_information_for_users' => 'cron#get_transfer_information_for_users'
   post '/cron/ground' => 'cron#ground'
 
-  resource :bank_admins, except: [:show, :update, :destroy, :create] do
-    scope module: :bank_admins do
-      post   "login"  => "sessions#create"
-      delete "logout" => "sessions#destroy"
-      resources :users, param: :bank_identifier 
-    end
-  end
-
-  resources :users, param: :bank_identifier, :only => [] do # We don't want unscoped user resource endpoints, hence the only hash points to no actions.
-    scope module: :bank_admins do
-      scope module: :users do
-        resources :demographics
-        resources :transfers, except: [:update, :destroy, :create]
-        resources :goals
-      end
+  scope module: :bank_admins do
+    post   "login"  => "sessions#create"
+    delete "logout" => "sessions#destroy"
+    resources :users, param: :bank_identifier do
+        scope module: :users do
+          resources :demographics
+          resources :transfers, except: [:update, :destroy, :create]
+          resources :goals
+        end
     end
   end
 end
