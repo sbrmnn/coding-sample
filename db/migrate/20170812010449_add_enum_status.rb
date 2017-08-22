ef0562bb15d55a4402676132e515998307ecf828
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-08-22 08:18:32 -0400

    changed some indentations

--- db/migrate/20170812010449_add_enum_status.rb
+++ db/migrate/20170812010449_add_enum_status.rb
@@ -1,6 +1,6 @@
 class AddEnumStatus < ActiveRecord::Migration[5.0]
   def self.up
-  	ActiveRecord::Base.connection.execute <<~SQL
+    ActiveRecord::Base.connection.execute <<~SQL
       CREATE TYPE status AS ENUM ('successful', 'pending', 'failed');
     SQL
   end
