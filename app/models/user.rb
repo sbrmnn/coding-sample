9462a4f8d06ae5bcec0acdd8c3f8ce3b02b57782
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-09-14 23:57:11 -0400

    Made sure transfer record was created when user is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
   has_many :demographics, dependent: :destroy
-  has_many :transfers, dependent: :destroy
+  has_one :transfer, dependent: :destroy
   has_many :goals, dependent: :destroy
-  
   belongs_to :financial_institution
-  
+
   before_save :verify_max_transfer_amount_for_user_is_equal_or_less_than_financial_institution_amount
-  
+  after_create :insert_transfer_record
   validates :max_transfer_amount, numericality: { greater_than_or_equal_to: 0}
-  validates_presence_of :financial_institution, :sequence, :bank_user_id, :savings_account_identifier, :checking_account_identifier
+  validates_presence_of :financial_institution, :sequence, :bank_user_id,
+                        :savings_account_identifier, :checking_account_identifier
   validates :bank_user_id, uniqueness: { scope: :financial_institution_id }
   
   protected
@@ -22,6 +22,11 @@ class User < ApplicationRecord
     else
       self.max_transfer_amount = 0 
     end
+  end
+
+  def insert_transfer_record
+    Transfer.create(user: self, next_transfer_date: nil, amount: 0,
+                    end_date: 'infinity')
   end
 end
 
