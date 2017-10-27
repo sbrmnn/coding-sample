c227a9c45a3958a1346f7ef47ea3705e54c3cc8d
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-10-27 18:13:15 -0400

    Changed param key

--- app/controllers/bank_admins/ads_controller.rb
+++ app/controllers/bank_admins/ads_controller.rb
@@ -50,7 +50,7 @@ class BankAdmins::AdsController < BankAdmins::ApplicationController
     if params[:ad].nil?
       {}
     else
-      params.require(:ad).permit(:header, :body, :link, :image_url)
+      params.require(:ad).permit(:header, :body, :link, :image)
     end
    end
 end
