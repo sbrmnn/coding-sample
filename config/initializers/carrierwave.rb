ec0618115a8189cb86f501e2e0b4d55a2a9509ae
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-10-27 16:08:35 -0400

    Uploader configuration for dev

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,17 @@
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        
-  config.fog_credentials = {
-    provider:              'AWS',                        
-    aws_access_key_id:     Rails.application.secrets.aws_access_key_id,                        
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key               
-  }
-  config.fog_directory  = 'monotto-images'                          
-  config.fog_public     = false                                        
-  config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } 
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     Rails.application.secrets.aws_access_key_id,
+      aws_secret_access_key: Rails.application.secrets.aws_secret_access_key
+    }
+    config.fog_directory  = 'monotto-images'
+    config.fog_public     = false
+    config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" }
+else
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
 end
-
-
-
