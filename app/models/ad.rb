d4ea4701e4944df1017550aa9f8f1c3169a8102a
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-10-28 12:59:17 -0400

    Added validations

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,4 +1,5 @@
 class Ad < ApplicationRecord
    mount_uploader :image, AdImageUploaderUploader
+   validates_presence_of :header, :body, :link
 end
 
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,5 +1,15 @@
 class Offer < ApplicationRecord
    attr_accessor :ad_header, :ad_body, :ad_link, :ad_image_url
+
    belongs_to :xref_goal_type
-   belongs_to :financial_instiution
+
+   belongs_to :financial_institution
+
+   validates_presence_of :name, :condition, :value
+
+   validates :symbol,
+    :inclusion  => { :in => [ '=', '>', '<', '>=', '<=' ],
+    :message    => "%{value} is not a valid symbol" }
+
+   validates :amount, numericality: { greater_than_or_equal_to: 0}
 end
