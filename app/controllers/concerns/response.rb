c679667ad1effe1ef9f307f70d9f1c1210dfa950
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-11-03 17:13:14 -0400

    Refactored response

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -15,14 +15,11 @@ module Response
     elsif record.try(:errors).present?
       object = record.errors
       status = :unprocessable_entity
-    elsif record.class.is_a?(ActiveRecord::Relation)
-      status = :ok
-      object = record.map{|rec| rec.class.reflect_on_all_associations.map{|l| l.name}}
     else
       status = :ok
       object = record  
-      associations = record.class.reflect_on_all_associations.map{|l| l.name}
+      assoc = record.class.is_a?(ActiveRecord::Relation) ? record.first.class.reflect_on_all_associations.map{|l| l.name} : record.class.reflect_on_all_associations.map{|l| l.name}
     end
-    render json: object.to_json(:include => associations), status: status
+    render json: object.to_json(:include => assoc), status: status
   end
 end
