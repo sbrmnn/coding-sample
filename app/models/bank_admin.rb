a506358a1cb98771464d1a54fcd2a30e5afd99f5
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-11-05 17:54:35 -0500

    Added relation to bank admin

--- app/models/bank_admin.rb
+++ app/models/bank_admin.rb
@@ -1,9 +1,10 @@
 class BankAdmin < ApplicationRecord
   include ModelAuthenticatable
-  has_many :users,    through: :financial_institution
-  has_many :offers,   through: :financial_institution
-  has_many :products, through: :financial_institution
-  has_many :ads,      through: :financial_institution
+  has_many :users,           through: :financial_institution
+  has_many :offers,          through: :financial_institution
+  has_many :products,        through: :financial_institution
+  has_many :ads,             through: :financial_institution
+  has_many :xref_goal_types, through: :financial_institution
  
   belongs_to :financial_institution
 
