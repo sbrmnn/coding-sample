fa4db79496fe160f2f48e00c5c8f26c6fd2a2987
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-11-06 22:02:55 -0500

    Changed access control from wildcard to localhost

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Monotto
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:3000'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
