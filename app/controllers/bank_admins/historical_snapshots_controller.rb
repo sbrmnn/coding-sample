456f4551b5db456e00cb5fa9502911f9b6fd5cc5
arun subramanian <ArunSubramanian@aruns-MBP.fios-router.home>
2017-11-23 18:06:01 -0500

    Made sure historical snapshots only returns for the pas 6 months

--- app/controllers/bank_admins/historical_snapshots_controller.rb
+++ app/controllers/bank_admins/historical_snapshots_controller.rb
@@ -1,6 +1,6 @@
 class BankAdmins::HistoricalSnapshotsController < ApplicationController
   def index
-    @historical_snapshot = current_bank_admin.financial_institution.try(:historical_snapshots)
+    @historical_snapshot = current_bank_admin.try(:historical_snapshots).where("date > ?", 6.months.ago)
     json_response(@historical_snapshot)
   end
 end
--- app/models/bank_admin.rb
+++ app/models/bank_admin.rb
@@ -1,11 +1,11 @@
 class BankAdmin < ApplicationRecord
   include ModelAuthenticatable
-  has_many :users,           through: :financial_institution
-  has_many :offers,          through: :financial_institution 
-  has_many :products,        through: :financial_institution
-  has_many :ads,             through: :financial_institution
-  has_many :xref_goal_types, through: :financial_institution
- 
+  has_many :users,                through: :financial_institution
+  has_many :offers,               through: :financial_institution
+  has_many :products,             through: :financial_institution
+  has_many :ads,                  through: :financial_institution
+  has_many :xref_goal_types,      through: :financial_institution
+  has_many :historical_snapshots, through: :financial_institution
   belongs_to :financial_institution
 
   validates_presence_of :financial_institution, :email, :name, :title, :phone
