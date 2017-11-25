22f75b5f4710753889596478747fa3e818db0c03
arun subramanian <ArunSubramanian@aruns-MBP.fios-router.home>
2017-11-24 20:06:32 -0500

    Added before_validation methods

--- app/models/xref_goal_type.rb
+++ app/models/xref_goal_type.rb
@@ -1,6 +1,19 @@
 class XrefGoalType < ApplicationRecord
+   before_validation :upcase_code
+   before_validation :humanize_and_titleize_name
    has_many :goals, dependent: :destroy
    belongs_to :financial_institution
    validates_uniqueness_of :name, :code, scope: :financial_institution_id
    validates_presence_of :name, :code
+
+
+   protected
+
+   def humanize_and_titleize_name
+     self.name = name.try(:humanize).try(:titleize)
+   end
+
+   def upcase_code
+     self.code = code.try(:upcase)
+   end
 end
