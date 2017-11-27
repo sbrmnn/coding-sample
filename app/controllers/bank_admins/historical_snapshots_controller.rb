a817c026eaedc8b71b8b9dd1e5ea0bbad8d4fdf2
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-11-27 01:35:11 -0500

    made sure historical data ascends in date

--- app/controllers/bank_admins/historical_snapshots_controller.rb
+++ app/controllers/bank_admins/historical_snapshots_controller.rb
@@ -1,6 +1,6 @@
 class BankAdmins::HistoricalSnapshotsController < ApplicationController
   def index
-    @historical_snapshot = current_bank_admin.try(:historical_snapshots).where("date > ?", 6.months.ago)
+    @historical_snapshot = current_bank_admin.try(:historical_snapshots).where("date > ?", 6.months.ago).order('date ASC')
     json_response(@historical_snapshot)
   end
 end
