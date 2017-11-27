db9bed607a8f2615636f5359080b97c67cc75e30
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-11-27 01:54:11 -0500

    Refactored controller

--- app/controllers/bank_admins/historical_snapshots_controller.rb
+++ app/controllers/bank_admins/historical_snapshots_controller.rb
@@ -1,6 +1,6 @@
-class BankAdmins::HistoricalSnapshotsController < ApplicationController
+class BankAdmins::HistoricalSnapshotsController < BankAdmins::ApplicationController
   def index
-    @historical_snapshot = current_bank_admin.try(:historical_snapshots).where("date > ?", 6.months.ago).order('date ASC')
+    @historical_snapshot = current_bank_admin.historical_snapshots.where("date > ?", 6.months.ago).order('date ASC')
     json_response(@historical_snapshot)
   end
 end
