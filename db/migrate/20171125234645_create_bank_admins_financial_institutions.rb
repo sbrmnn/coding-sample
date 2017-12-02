ed525f81c3d67d97e133b4ddfab27df150257e8e
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2017-12-02 15:33:59 -0500

    Deleted migration

--- db/migrate/20171125234645_create_bank_admins_financial_institutions.rb
+++
@@ -1,8 +0,0 @@
-class CreateBankAdminsFinancialInstitutions < ActiveRecord::Migration[5.0]
-  def change
-    create_table :bank_admins_financial_institutions do |t|
-
-      t.timestamps
-    end
-  end
-end
