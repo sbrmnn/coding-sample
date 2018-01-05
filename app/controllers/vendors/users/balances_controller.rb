3ee53fd54cf6a24f54d411379171cc94ac9e8198
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-04 22:33:52 -0500

    Refactored controller

--- app/controllers/vendors/users/balances_controller.rb
+++ app/controllers/vendors/users/balances_controller.rb
@@ -1,4 +1,7 @@
-class Vendors::Users::BalancesController < ApplicationController
+class Vendors::Users::BalancesController < Vendors::ApplicationController
+  skip_before_action :require_vendor_login
+  before_action :find_user_by_vendor_public_key
+
   def show
     records = @user.goals.pluck(:savings_account_identifier, :savings_acct_balance).uniq
     @savings_balance = {savings_balance: records.map{|r| r[1]}.sum}
