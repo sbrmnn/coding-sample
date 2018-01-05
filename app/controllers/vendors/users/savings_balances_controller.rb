f0beb4aa70abf0280805eb65083acaef466edc3c
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-04 22:24:54 -0500

    got rid of code

--- app/controllers/vendors/users/savings_balances_controller.rb
+++ app/controllers/vendors/users/savings_balances_controller.rb
@@ -3,8 +3,8 @@ class Vendors::Users::SavingsBalancesController < ApplicationController
   before_action :find_user_by_vendor_public_key
 
   def show
-    records = @user.goals.pluck(:savings_account_identifier, :savings_acct_balance).uniq
-    @savings_balance = {savings_balance: records.map{|r| r[1]}.sum}
-    json_response(@savings_balance)
+    #records = @user.goals.pluck(:savings_account_identifier, :savings_acct_balance).uniq
+    #@savings_balance = {savings_balance: records.map{|r| r[1]}.sum}
+    #json_response(@savings_balance)
   end
 end
