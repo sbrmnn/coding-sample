e4725597da270ecb35499acaac8e75634a992a92
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-13 10:43:25 -0500

    Added unique validation to message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :message_obj, polymorphic: true
   belongs_to :user
+  validates_uniqueness_of :message_obj_id, :scope => [:message_obj_type, :user_id]
 end
