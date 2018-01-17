38bab85fab0868b6cc5ba8bd4f78dda78fb0cd19
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-17 12:23:35 -0500

    Made sure previously stored image is not removed

--- app/models/ad.rb
+++ app/models/ad.rb
@@ -1,5 +1,6 @@
 class Ad < ApplicationRecord
    mount_uploader :image, AdImageUploader
+   skip_callback :commit, :after, :remove_previously_stored_image
    has_many :offers, dependent: :destroy
    belongs_to :financial_institution
    validates_presence_of :header, :body, :link, :name
