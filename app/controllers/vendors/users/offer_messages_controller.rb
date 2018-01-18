7887724f9e9480e84454c443d2e0f2b9a9deb316
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-17 23:39:09 -0500

    Reloaded instance variable

--- app/controllers/vendors/users/offer_messages_controller.rb
+++ app/controllers/vendors/users/offer_messages_controller.rb
@@ -1,11 +1,12 @@
 class Vendors::Users::OfferMessagesController < Vendors::ApplicationController
   skip_before_action :require_vendor_login
   before_action :find_user_by_vendor_key
-  
+
   def update
   	@offer_message = @user.messages.find_by(message_obj_id: params[:offer_id], message_obj_type: :Offer)
     if @offer_message
       Message.increment_counter(:clicks, @offer_message.id)
+      @offer_message.reload
     end
     json_response(@offer_message)
   end
