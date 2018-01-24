499c3aa0b7ceb2fdd99b54d3bbfb6954aeb620ce
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-24 17:25:37 -0500

    Changed find method

--- app/controllers/vendors/application_controller.rb
+++ app/controllers/vendors/application_controller.rb
@@ -5,7 +5,7 @@ class Vendors::ApplicationController < ApplicationController
 
   def find_financial_institution_by_vendor_key
     @vendor = Vendor.find_by(key: params[:vendor_key].try(:strip))
-    @financial_institution = @vendor.try(:financial_institutions)&.find(params[:financial_institution_id])
+    @financial_institution = @vendor.try(:financial_institutions)&.find_by(id: params[:financial_institution_id])
     if @vendor.blank?
       json_response({:vendor => :not_found}, nil, :not_found) and return
     elsif @financial_institution.blank?
