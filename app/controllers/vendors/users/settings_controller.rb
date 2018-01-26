23053fa482e930e032a93335d0216b9659cd0083
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-26 18:10:34 -0500

    Refactor 2

--- app/controllers/vendors/users/settings_controller.rb
+++ app/controllers/vendors/users/settings_controller.rb
@@ -3,30 +3,24 @@ class Vendors::Users::SettingsController < Vendors::ApplicationController
   before_action :find_user_by_vendor_key
   
   def show
-    @user&.slice(allows_columns)
+    @user&.slice(:default_savings_account_identifier, :checking_account_identifier,
+                                  :transfers_active, :safety_net_active, :max_transfer_amount)
     json_response(@user)
   end
 
   def update
     @user.update_attributes(user_params)
-    json_response(@user)
+    json_response(@user) 
   end
 
   protected
-
+  
   def user_params
-   if params[:user].blank?
+    if params[:user].blank?
      {}
-   else
-     params.require(:user).permit(allows_columns)
-   end
+    else
+     params.require(:user).permit(:default_savings_account_identifier, :checking_account_identifier,
+                                  :transfers_active, :safety_net_active, :max_transfer_amount)
+    end
   end
-
-  def allows_columns
-    [ 
-      :default_savings_account_identifier, :checking_account_identifier,
-      :transfers_active, :safety_net_active, :max_transfer_amount
-    ]
-  end
-
 end
