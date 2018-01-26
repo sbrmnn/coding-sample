00716a56e14193d38a99ff89e98d97531b241f2a
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-01-26 17:54:32 -0500

    Added settings controller

---
+++ app/controllers/vendors/users/settings_controller.rb
@@ -0,0 +1,21 @@
+class Vendors::Users::SettingsController < Vendors::ApplicationController
+  skip_before_action :require_vendor_login
+  before_action :find_user_by_vendor_key
+  
+  def show
+   json_response(@user)
+  end
+
+  def update
+   @user.update_attributes(user_params)
+   json_response(@user) 
+  end
+  def user_params
+   if params[:user].blank?
+     {}
+   else
+     params.require(:user).permit(:default_savings_account_identifier, :checking_account_identifier,
+                                  :transfers_active, :safety_net_active, :max_transfer_amount)
+   end
+  end
+end
