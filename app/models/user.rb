5a462a329c8470394aafd4ec1b90028d647104ac
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-02-01 01:34:38 -0500

    Changed places for login_bank_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,13 +37,11 @@ class User < ApplicationRecord
     if resp["Status"] == 'Failure'
       self.api_errors << ApiError.new(status: resp["Status"], response: resp["Reason"], service: :aws_lambda, function: :registration)
     elsif resp["Status"] == 'Success'
-      login_bankjoy_user
-
       savings_acct_balance = Goal.where(savings_account_identifier: default_savings_account_identifier, user_id: id).first.try(:savings_acct_balance).to_i
       goal = Goal.new(tag: "Safety Net", xref_goal_name: "Other Goal", financial_institution: financial_institution, 
-                      priority: 1,  target_amount: resp["Result"]["safety_net"].to_i, savings_account_identifier: default_savings_account_identifier,
-                      savings_acct_balance: savings_acct_balance)
+                      priority: 1,  target_amount: resp["Result"]["safety_net"].to_i)
       self.goals << goal
+      login_bankjoy_user
       # Any previous errors associated with the registration should be removed if successful.
       self.api_errors.where(service: :aws_lambda, function: :registration).destroy_all
     end
