2133029eccb557950cfcba5ac33119b10ff3c28c
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-02-13 19:53:51 -0500

    Added validation

--- app/models/recurring_transfer_rule.rb
+++ app/models/recurring_transfer_rule.rb
@@ -2,4 +2,6 @@ class RecurringTransferRule < ApplicationRecord
   has_many :transfers
   belongs_to :goal
   validates_presence_of :amount, :start_dt, :end_dt, :frequency, :repeats
+  enum periods: [:day, :week, :month]
+  validates :frequency, inclusion: { in: periods.keys }
 end
