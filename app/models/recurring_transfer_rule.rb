a04f04adbeb052eeeb4ee59229718a73db349d51
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-02-14 12:18:42 -0500

    Got rid of null

--- app/models/recurring_transfer_rule.rb
+++ app/models/recurring_transfer_rule.rb
@@ -2,7 +2,7 @@ class RecurringTransferRule < ApplicationRecord
   has_many :transfers
   belongs_to :goal
   validates_presence_of :amount, :start_dt, :end_dt, :frequency, :repeats
-  enum periods: [:day, :week, :month, :null]
+  enum periods: [:day, :week, :month]
   validates :frequency, inclusion: { in: periods.keys }
   acts_as_paranoid
 end
