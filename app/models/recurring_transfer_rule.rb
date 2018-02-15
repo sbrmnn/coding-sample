2d81ac1f60f41c7f732893b54a53f9528cb6cbfe
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-02-15 16:32:05 -0500

    Changed enum value

--- app/models/recurring_transfer_rule.rb
+++ app/models/recurring_transfer_rule.rb
@@ -2,7 +2,7 @@ class RecurringTransferRule < ApplicationRecord
   has_many :transfers
   belongs_to :goal
   validates_presence_of :amount, :start_dt, :frequency, :repeats
-  enum periods: [:days, :week, :month]
+  enum periods: [:day, :week, :month]
   validates :frequency, inclusion: { in: periods.keys }
   acts_as_paranoid
 end
