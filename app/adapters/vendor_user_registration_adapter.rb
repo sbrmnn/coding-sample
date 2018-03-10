ecf9e28d99a69f3212ead9b80158a3b484c70dc5
Arun Shankar Subramanian <gth824c@yahoo.com>
2018-03-10 16:11:39 -0500

    Rearranged code

--- app/adapters/vendor_user_registration_adapter.rb
+++ app/adapters/vendor_user_registration_adapter.rb
@@ -22,12 +22,12 @@ module VendorUserRegistrationAdapter
       if resp["Status"] == 'Failed'
         user.api_errors << ApiError.new(status: resp["Status"], response: resp["Reason"], service: :aws_lambda, function: :registration)
       elsif resp["Status"] == 'Success'
+        # Any previous errors associated with the registration should be removed if successful.
+        user.api_errors.where(service: :aws_lambda, function: :registration).destroy_all
         goal = Goal.new(tag: "Safety Net", xref_goal_name: "Other Goal", financial_institution: user.financial_institution,
                         priority: 1,  target_amount: resp["Result"]["safety_net"].to_f)
         user.goals << goal
         VendorUserLoginAdapter::BankJoy.vendor_login(user_id)
-        # Any previous errors associated with the registration should be removed if successful.
-        user.api_errors.where(service: :aws_lambda, function: :registration).destroy_all
       end
     end
   end
