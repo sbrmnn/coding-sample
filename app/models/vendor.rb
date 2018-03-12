96bba9f3c9a5e39c0e0ef0eb9d716ca9180dbbee
arun subramanian <ArunSubramanian@aruns-MacBook-Pro.local>
2018-03-12 09:53:20 -0400

    removed key

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,26 +3,11 @@ class Vendor < ApplicationRecord
   has_many :users, through: :financial_institutions
   has_many :financial_institutions
   has_many :vendor_user_keys
-  before_save :create_key
   validates_presence_of :name, :location, :email
   validates_uniqueness_of :email
 
 
   def bankjoy_vendor?
     self.name.try(:downcase) == 'bankjoy'
-  end
-
-
-  protected
-
-  def create_key
-    if self.key.blank?
-      token_length = 50
-      token = SecureRandom.urlsafe_base64(token_length)
-      while Vendor.exists?(:key => token) # Making sure token hasn't been assigned for another vendor.
-        token = SecureRandom.urlsafe_base64(token_length)
-      end
-      self.key = token
-    end
   end
 end
